b7c99ca4f4de0f1be8c1da73af05c2de0c776350
Jo <jo@pop-os.localdomain>
2021-03-11 22:19:09 +0000

    Full BetterFolders Plugin compatability

--- index.js
+++ index.js
@@ -15,15 +15,19 @@ module.exports = class ScalableDiscord extends Plugin {
 			try {
 
 				a.classList.forEach((b) => {
-						const channelBar = document.querySelector('.sidebar-2K8pFh');
+						const channelBar = document.querySelector('.sidebar-2K8pFh');						
 						const chatBar = document.querySelector('.chat-3bRxxu');
 						const serverBar = document.querySelector('.guilds-1SWlCJ');
+  					const serverBarBETTERFOLDERS = document.querySelector('.BF-folderSidebar > .guilds-1SWlCJ');
 						const serverBarChild = document.querySelector('.tree-3tCaw8');
-
+            const serverBarChildBETTERFOLDERS = document.querySelector('.BF-folderSidebar > * > .tree-3tCaw8');
+						
 					if (b === "content-1x5b-n" || b === "channel-2QD9_O") {
 						channelBar.classList.remove('channel-shown');
-						serverBarChild.classList.remove('server-disabled');
+						serverBarChild.classList.remove('server-disabled');						
+						serverBarChildBETTERFOLDERS.classList.remove('server-disabled');
 						serverBar.classList.remove('undarken');
+						serverBarBETTERFOLDERS.classList.remove('undarken');
 						chatBar.classList.remove('chat-hidden');
 						channelBar.classList.remove('channel-stretched');
 					}
@@ -31,6 +35,8 @@ module.exports = class ScalableDiscord extends Plugin {
 						channelBar.classList.add('channel-shown');
 						serverBarChild.classList.add('server-disabled');
 						serverBar.classList.add('undarken');
+						serverBarChildBETTERFOLDERS.classList.add('server-disabled');
+						serverBarBETTERFOLDERS.classList.add('undarken');
 						chatBar.classList.add('chat-hidden');
 						channelBar.classList.add('channel-stretched');
 					}
