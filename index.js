90bf8852c6a486ce7b985ec74d6343fd7b36439e
jo! <jo@pop-os.local>
2021-03-31 23:14:46 +0100

    I MADE FRIENDS WORK I MADE NITRO WORK I MADE USER SETTINGS WORK ITS ALL COMING TOGETHER

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { Plugin } = require('powercord/entities');
 
+// TODO: fix friends list animations
+// TODO: fix nitro page animations
+
 module.exports = class ScalableDiscord extends Plugin {
 
 	startPlugin() {
@@ -19,23 +22,45 @@ module.exports = class ScalableDiscord extends Plugin {
 		element.path.forEach((a) => {
 			try {
 				a.classList.forEach((b) => {
-						const channelBar = document.querySelector('.sidebar-2K8pFh');						
+						const channelBar = document.querySelector('.sidebar-2K8pFh');
 						const chatBar = document.querySelector('.chat-3bRxxu');
+						const friendsBar = document.querySelector('.container-1D34oG');
 						const serverBar = document.querySelector('.guilds-1SWlCJ');
-  			//		const serverBarBETTERFOLDERS = document.querySelector('.BF-folderSidebar > .guilds-1SWlCJ');
 						const serverBarChild = document.querySelector('.tree-2wKJdG');
-          //  const serverBarChildBETTERFOLDERS = document.querySelector('.BF-folderSidebar > * > .tree-3tCaw8');
-						const settings_sidebar = document.querySelector('.sidebarRegion-VFTUkN');
-						const settings_content = document.querySelector('.contentRegion-3nDuYy');
-						const settings_wrapper = document.querySelector('.layer-3QrUeG[aria-label="USER_SETTINGS"]')
+						// const settings_sidebar = document.querySelector('.sidebarRegion-VFTUkN');
+						// const settings_content = document.querySelector('.contentRegion-3nDuYy');
+						const settings_wrapper = document.querySelector(':not(.baseLayer-35bLyl).layer-3QrUeG');
+
+					// SHOW CHAT
 					if (b === "content-1x5b-n" || b === "channel-2QD9_O") {
 						channelBar.classList.remove('channel-shown');
+						channelBar.classList.remove('channel-stretched');
 						serverBarChild.classList.remove('server-disabled');						
 						//serverBarChildBETTERFOLDERS.classList.remove('server-disabled');
 						serverBar.classList.remove('undarken');
 						//serverBarBETTERFOLDERS.classList.remove('undarken');
-						chatBar.classList.remove('chat-hidden');
-						channelBar.classList.remove('channel-stretched');
+
+						// FUNCTION ENDS HERE IF CHATBAR DOESNT EXIST (FRIENDS LIST)
+						// hopefully fixed by this?
+						if (document.body.contains(chatBar)) {
+							chatBar.classList.remove('chat-hidden');
+						} else if (document.body.contains(friendsBar)) {
+							friendsBar.classList.remove('chat-hidden');
+						}
+
+					// SHOW CHANNELS
+					} else if (b === "guilds-1SWlCJ") {
+						channelBar.classList.add('channel-shown');
+						channelBar.classList.add('channel-stretched');
+						serverBarChild.classList.add('server-disabled');
+						serverBar.classList.add('undarken');
+
+						// FUNCTION ENDS HERE IF CHATBAR DOESNT EXIST (FRIENDS LIST)
+						if (document.body.contains(chatBar)) {
+							chatBar.classList.add('chat-hidden');
+						} else if (document.body.contains(friendsBar)) {
+							friendsBar.classList.add('chat-hidden');
+						}
 					} else if (b === "item-PXvHYJ") {
 						settings_wrapper.classList.add('settings-contentshowing');
 						lock = true;
@@ -44,14 +69,7 @@ module.exports = class ScalableDiscord extends Plugin {
 						if (lock === false) {
 							settings_wrapper.classList.remove('settings-contentshowing');
 						}
-					} else if (b === "guilds-1SWlCJ") {
-						channelBar.classList.add('channel-shown');
-						serverBarChild.classList.add('server-disabled');
-						serverBar.classList.add('undarken');
-						//serverBarChildBETTERFOLDERS.classList.add('server-disabled');
-						//serverBarBETTERFOLDERS.classList.add('undarken');
-						chatBar.classList.add('chat-hidden');
-						channelBar.classList.add('channel-stretched');
+
 					}
 
 				}
